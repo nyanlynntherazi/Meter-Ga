5717d7c90f7932a884c561c12e23456afc3283d5
Nyan Lynn Myint <nyanlynn.razi@gmail.com>
2024-08-30 21:57:37 +0630

    Update function.js

--- function.js
+++ function.js
@@ -4,24 +4,24 @@ const calculatePriceNew = (quantity) => {
   
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 50);
-    totalCost += units * 50;  // $50 for the first 50 units
+    totalCost += units * 50;  // 50 Kyat for the first 50 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 50);
-    totalCost += units * 100;  // $100 for the next 50 units
+    totalCost += units * 100;  // 100 Kyat for the next 50 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 100);
-    totalCost += units * 150;  // $150 for the next 100 units
+    totalCost += units * 150;  // 150 Kyat for the next 100 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
-    totalCost += remainingQuantity * 300;  // $300 for anything beyond 200 units
+    totalCost += remainingQuantity * 300;  // 300 Kyat for anything beyond
   }
   
   return totalCost;
@@ -35,41 +35,41 @@ const calculatePriceOld = (quantity) => {
   
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 30);
-    totalCost += units * 35;  // $50 for the first 50 units
+    totalCost += units * 35;  // 35 Kyat for the first 30 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 20);
-    totalCost += units * 50;  // $100 for the next 50 units
+    totalCost += units * 50;  // 50 Kyat for the next 20 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 25);
-    totalCost += units * 70;  // $150 for the next 100 units
+    totalCost += units * 70;  // 70 Kyat for the next 25 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 25);
-    totalCost += units * 90;  // $150 for the next 100 units
+    totalCost += units * 90;  // 90 Kyat for the next 25 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 50);
-    totalCost += units * 110;  // $150 for the next 100 units
+    totalCost += units * 110;  // 110 Kyat for the next 50 units
     remainingQuantity -= units;
   } 
   if (remainingQuantity > 0) {
     let units = Math.min(remainingQuantity, 50);
-    totalCost += units * 120;  // $150 for the next 100 units
+    totalCost += units * 120;  // 120 Kyat for the next 50 units
     remainingQuantity -= units;
   }
 
   if (remainingQuantity > 0) {
-    totalCost += remainingQuantity * 125;  // $300 for anything beyond 200 units
+    totalCost += remainingQuantity * 125;  // 125 for anything beyond
   }
   
   return totalCost;
